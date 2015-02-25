60e59cd5c573930e3efbd307fcb3dea73398f13a
Andrei Daldegan <andrei.daldegan@gmail.com>
2015-02-25 00:39:25 -0300

    FIX: Using viewport instead camera to draw parallax

--- core/animvs-core/src/br/com/animvs/engine2/graficos/parallax/ParallaxBackground.java
+++ core/animvs-core/src/br/com/animvs/engine2/graficos/parallax/ParallaxBackground.java
@@ -1,17 +1,16 @@
 package br.com.animvs.engine2.graficos.parallax;
 
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /**
  * Created by DALDEGAN on 24/02/2015.
  */
 public final class ParallaxBackground implements Disposable {
     private ParallaxLayer[] layers;
-    private Camera camera;
+    private Viewport viewport;
     private SpriteBatch batch;
     private Vector2 speed = new Vector2();
 
@@ -21,37 +20,41 @@ public final class ParallaxBackground implements Disposable {
      * @param height The screenHeight
      * @param speed  A Vector2 attribute to point out the x and y speed
      */
-    public ParallaxBackground(ParallaxLayer[] layers, float width, float height, Vector2 speed, Camera camera) {
+    public ParallaxBackground(ParallaxLayer[] layers, float width, float height, Vector2 speed, Viewport viewport) {
         this.layers = layers;
         this.speed.set(speed);
 
-        if (camera == null)
-            this.camera = new OrthographicCamera(width, height);
-        else
-            this.camera = camera;
+        if (viewport == null)
+            throw new RuntimeException("The parameter 'viewport' must be != NULL");
 
+        this.viewport = viewport;
         batch = new SpriteBatch();
     }
 
     public void render(float delta) {
-        this.camera.position.add(speed.x * delta, speed.y * delta, 0);
-        for (ParallaxLayer layer : layers) {
-            batch.setProjectionMatrix(camera.projection);
+        viewport.getCamera().position.add(speed.x * delta, speed.y * delta, 0f);
+        for (int i = 0; i < layers.length; i++) {
+            batch.setProjectionMatrix(viewport.getCamera().projection);
             batch.begin();
-            float currentX = -camera.position.x * layer.parallaxRatio.x % (layer.region.getRegionWidth() + layer.padding.x);
+            float currentX = -viewport.getCamera().position.x * layers[i].parallaxRatio.x % (layers[i].region.getRegionWidth() + layers[i].padding.x);
 
-            if (speed.x < 0) currentX += -(layer.region.getRegionWidth() + layer.padding.x);
+            if (speed.x < 0) currentX += -(layers[i].region.getRegionWidth() + layers[i].padding.x);
             do {
-                float currentY = -camera.position.y * layer.parallaxRatio.y % (layer.region.getRegionHeight() + layer.padding.y);
-                if (speed.y < 0) currentY += -(layer.region.getRegionHeight() + layer.padding.y);
+                float currentY = -viewport.getCamera().position.y * layers[i].parallaxRatio.y % (layers[i].region.getRegionHeight() + layers[i].padding.y);
+
+                if (speed.y < 0)
+                    currentY += -(layers[i].region.getRegionHeight() + layers[i].padding.y);
+
                 do {
-                    batch.draw(layer.region,
-                            -this.camera.viewportWidth / 2 + currentX + layer.startPosition.x,
-                            -this.camera.viewportHeight / 2 + currentY + layer.startPosition.y);
-                    currentY += (layer.region.getRegionHeight() + layer.padding.y);
-                } while (currentY < camera.viewportHeight);
-                currentX += (layer.region.getRegionWidth() + layer.padding.x);
-            } while (currentX < camera.viewportWidth);
+                    batch.draw(layers[i].region,
+                            -viewport.getCamera().viewportWidth / 2 + currentX + layers[i].startPosition.x,
+                            -viewport.getCamera().viewportHeight / 2 + currentY + layers[i].startPosition.y);
+
+                    currentY += (layers[i].region.getRegionHeight() + layers[i].padding.y);
+                } while (currentY < viewport.getCamera().viewportHeight);
+
+                currentX += (layers[i].region.getRegionWidth() + layers[i].padding.x);
+            } while (currentX < viewport.getCamera().viewportWidth);
             batch.end();
         }
     }
