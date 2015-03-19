b4857998088af109d5e7be069607771270087a83
Andrei Daldegan <andrei.daldegan@gmail.com>
2015-03-19 15:25:32 -0300

    FIX: Git craziness

--- core/animvs-core/src/br/com/animvs/engine2/console/commands/CommandSender.java
+++
@@ -1,8 +0,0 @@
-package br.com.animvs.engine2.console.commands;
-
-/**
- * Created by DALDEGAN on 19/03/2015.
- */
-public interface CommandSender {
-    void executeEventBatch(String eventName, String commandBatchStr);
-}
