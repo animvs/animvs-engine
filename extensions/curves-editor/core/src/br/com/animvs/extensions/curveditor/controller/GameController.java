0e92535f1e512e0a927076275f936d5d382a3b8e
Andrei Daldegan <andrei.daldegan@gmail.com>
2015-03-27 18:22:30 -0300

    ADD: refactored curve editor to use BSpline

--- extensions/curves-editor/core/src/br/com/animvs/extensions/curveditor/controller/GameController.java
+++ extensions/curves-editor/core/src/br/com/animvs/extensions/curveditor/controller/GameController.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.CatmullRomSpline;
+import com.badlogic.gdx.math.BSpline;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
@@ -21,8 +21,9 @@ public final class GameController implements Disposable {
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
 
-    private CatmullRomSpline<Vector2> bezier;
-    private Vector2[] points;
+    private BSpline<Vector2> curve;
+    private Vector2[] curveControlPoints;
+    private Vector2[] cachedPoints;
 
     private OrthographicCamera camera;
 
@@ -42,41 +43,37 @@ public final class GameController implements Disposable {
 
         input();
 
-        if (bezier == null || bezier.controlPoints.length <= 1)
+        if (curve == null || curve.controlPoints.length <= 1)
             return;
 
         this.shapeRenderer.setColor(0f, 0f, 1f, 1f);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         for (int i = 0; i < SPILINE_DETAIL - 1; ++i)
-            shapeRenderer.line(points[i], points[i + 1]);
+            shapeRenderer.line(cachedPoints[i], cachedPoints[i + 1]);
         shapeRenderer.end();
 
         this.shapeRenderer.setColor(1f, 0f, 0f, 1f);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        for (int i = 0; i < bezier.controlPoints.length; i++)
-            shapeRenderer.circle(bezier.controlPoints[i].x, bezier.controlPoints[i].y, 5f);
+        for (int i = 0; i < curveControlPoints.length; i++)
+            shapeRenderer.circle(curveControlPoints[i].x, curveControlPoints[i].y, 5f);
         shapeRenderer.end();
 
         /* ON THE FLY
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.getColor().set(0f, 0f, 1f, 1f);
         for (int i = 0; i < -1; ++i)
-            shapeRenderer.line(bezier.valueAt(points[i], ((float) i) / ((float) SPILINE_DETAIL - 1)), bezier.valueAt(points[i + 1], ((float) i) / ((float) SPILINE_DETAIL - 1)));
+            shapeRenderer.line(curve.valueAt(cachedPoints[i], ((float) i) / ((float) SPILINE_DETAIL - 1)), curve.valueAt(cachedPoints[i + 1], ((float) i) / ((float) SPILINE_DETAIL - 1)));
         shapeRenderer.end();*/
     }
 
     private void cache() {
-        if (bezier.controlPoints.length <= 1)
+        if (curveControlPoints.length <= 1)
             return;
 
-        points = new Vector2[SPILINE_DETAIL];
-
-        //Bezier<Vector2> myCatmull = new Bezier<Vector2>(points, 0, 4);
-        //Vector2 out = new Vector2();
+        cachedPoints = new Vector2[SPILINE_DETAIL];
         for (int i = 0; i < SPILINE_DETAIL; ++i) {
-            points[i] = new Vector2();
-            //bezier.valueAt(points[i], ((float) i) / ((float) SPILINE_DETAIL - 1));
-            bezier.valueAt(points[i], i / (float) SPILINE_DETAIL);
+            cachedPoints[i] = new Vector2();
+            curve.valueAt(cachedPoints[i], i / (float) SPILINE_DETAIL);
         }
     }
 
@@ -89,8 +86,8 @@ public final class GameController implements Disposable {
 
         Array<Vector2> pointArray;
 
-        if (bezier != null)
-            pointArray = new Array<Vector2>(bezier.controlPoints);
+        if (curveControlPoints != null && curveControlPoints.length > 0)
+            pointArray = new Array<Vector2>(curveControlPoints);
         else
             pointArray = new Array<Vector2>(new Vector2[0]);
 
@@ -114,11 +111,15 @@ public final class GameController implements Disposable {
         cache();
     }
 
-    private void prepareCurve(Vector2[] points) {
-        if (bezier == null)
-            bezier = new CatmullRomSpline<Vector2>(points, false);
+    private void prepareCurve(Vector2[] newPoints) {
+        this.curveControlPoints = newPoints;
+        if (newPoints.length <= 1)
+            return;
+
+        if (curve == null)
+            curve = new BSpline<Vector2>(newPoints, 1, true);
         else
-            bezier.set(points, false);
+            curve.set(newPoints, 1, false);
     }
 
     @Override
